b7c5af3943e10fd775189c54103722bef1354b2b
EmanuelGustafzon <emanuel.gustafzon@gmail.com>
2022-08-16 14:18:34 +0000

    First challange done

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import react from 'react'
+
+const Sidebar = ()  => {
+    return (
+    <div>
+        <a href="#" target="_blank">My Photos</a>
+        <a href="#" target="_blank">My illustrations</a>
+        <a href="#" target="_blank">My paintings</a>
+    </div>)
+}
+
+
+export default Sidebar;
